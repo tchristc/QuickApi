0b5e8bb9937d92bf28c65190e5c7d6e81afa14b1
Tom <tom.c.christensen@gmail.com>
2018-12-25 19:51:05 -0600

    Adding db project

--- src/QuickApi/QuickApi.Database/dbo/Tables/EntityColumn.sql
+++ src/QuickApi/QuickApi.Database/dbo/Tables/EntityColumn.sql
@@ -6,6 +6,6 @@
 	[EntityColumnTypeId]	INT				NOT NULL,
 	[EntityColumnSize]		INT				NULL,
     CONSTRAINT [PK_EntityColumn] PRIMARY KEY ([EntityColumnId]),
-    CONSTRAINT [PK_EntityColumn_EntityTable] FOREIGN KEY ([EntityTableId]) REFERENCES [dbo].[EntityTable] (EntityTableId)
-    CONSTRAINT [PK_EntityColumn_EntityColumnType] FOREIGN KEY ([EntityColumnTypeId]) REFERENCES [dbo].[EntityColumnType] ([EntityColumnTypeId)
+    CONSTRAINT [PK_EntityColumn_EntityTable] FOREIGN KEY ([EntityTableId]) REFERENCES [dbo].[EntityTable] ([EntityTableId]),
+    CONSTRAINT [PK_EntityColumn_EntityColumnType] FOREIGN KEY ([EntityColumnTypeId]) REFERENCES [dbo].[EntityColumnType] ([EntityColumnTypeId])
 )
